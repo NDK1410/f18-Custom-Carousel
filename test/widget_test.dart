c172cf1feebd3f311fab0a541a17b5889b8859f8
NDK1410 <khanh.nd1410@gmail.com>
2020-11-17 02:50:11 -0800

    runApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:F18CustomCarousel/main.dart';
+import 'package:F18CustomCarousel/src/MyApp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
